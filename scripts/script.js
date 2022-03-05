b09c81bf8e1e3920a6a0dc4147170b1bce1aa32d
h-nriquevieira <h.nriquevieira@gmail.com>
2022-03-05 15:23:33 -0300

    Announce winner and draw

--- scripts/script.js
+++ scripts/script.js
@@ -21,10 +21,17 @@ const gameBoard = (function(){
   }
 
   function updateCurrentState(play) {
-    currentState[play] = 'o';
+    currentState[play] = 'x';
   }
 
-  return {currentState, gameboardDomCells, updateBoard, updateCurrentState}
+  function resetBoard() {
+    for (let i = 0; i < currentState.length; i++) {
+      currentState[i] = '';
+    }
+    updateBoard();
+  }
+
+  return {currentState, gameboardDomCells, updateBoard, updateCurrentState, resetBoard}
 })();
 
 const gameFlow = (function() {
@@ -34,13 +41,38 @@ const gameFlow = (function() {
     checkIfOver();
   }
 
+  function announceDraw() {
+    console.log('It\'s a draw!');
+  }
+
+  function checkWinner(i) {
+    switch (gameBoard.currentState[i]) {
+      case 'x':
+        return 'player';
+      case 'o':
+        return 'pc';
+    }
+  }
+
+  function announceWinner(winner) {
+    switch (winner) {
+      case 'player':
+        console.log('You win!');
+        break;
+      case 'pc':
+        console.log('PC wins!');
+        break;
+    }
+  }
+
   function checkIfOver() {  
     let counter = 0
     for (let i = 0; i<gameBoard.currentState.length; i++) {
       if (gameBoard.currentState[i]) {counter++}
     } 
     if (counter === gameBoard.currentState.length) {
-      console.log('draw');
+      announceDraw();
+      gameBoard.resetBoard();
       return;
     }
 
@@ -48,51 +80,89 @@ const gameFlow = (function() {
     if (gameBoard.currentState[0] && gameBoard.currentState[1] && gameBoard.currentState[2]) {
       if (gameBoard.currentState[0] === gameBoard.currentState[1] && gameBoard.currentState[0] === gameBoard.currentState[2]) {
         console.log('game over');
+        let winner = checkWinner(0);
+        announceWinner(winner);
+        gameBoard.resetBoard();
         return;
       }
     }
-    console.log('here')
+
     if (gameBoard.currentState[3] && gameBoard.currentState[4] && gameBoard.currentState[5]) {
       if (gameBoard.currentState[3] === gameBoard.currentState[4] && gameBoard.currentState[3] === gameBoard.currentState[5]) {
         console.log('game over');
+        let winner = checkWinner(3);
+        announceWinner(winner);
+        gameBoard.resetBoard();
+        return;
+        return;
       }
     } 
     
-    // if (gameBoard.currentState[6] && gameBoard.currentState[7] && gameBoard.currentState[8]) {
-    //   if (gameBoard.currentState[6] === gameBoard.currentState[7] && gameBoard.currentState[6] === gameBoard.currentState[8]) {
-    //     console.log('game over');
-    //   }
-    // } 
+    if (gameBoard.currentState[6] && gameBoard.currentState[7] && gameBoard.currentState[8]) {
+      if (gameBoard.currentState[6] === gameBoard.currentState[7] && gameBoard.currentState[6] === gameBoard.currentState[8]) {
+        console.log('game over');
+        let winner = checkWinner(6);
+        announceWinner(winner);
+        gameBoard.resetBoard();
+        return;
+        return;
+      }
+    } 
 
-    // if (gameBoard.currentState[0] && gameBoard.currentState[3] && gameBoard.currentState[6]) {
-    //   if (gameBoard.currentState[0] === gameBoard.currentState[3] && gameBoard.currentState[0] === gameBoard.currentState[6]) {
-    //     console.log('game over');
-    //   }
-    // } 
+    if (gameBoard.currentState[0] && gameBoard.currentState[3] && gameBoard.currentState[6]) {
+      if (gameBoard.currentState[0] === gameBoard.currentState[3] && gameBoard.currentState[0] === gameBoard.currentState[6]) {
+        console.log('game over');
+        let winner = checkWinner(0);
+        announceWinner(winner);
+        gameBoard.resetBoard();
+        return;
+        return;
+      }
+    } 
 
-    // if (gameBoard.currentState[1] && gameBoard.currentState[4] && gameBoard.currentState[7]) {
-    //   if (gameBoard.currentState[1] === gameBoard.currentState[4] && gameBoard.currentState[1] === gameBoard.currentState[7]) {
-    //     console.log('game over');
-    //   }
-    // } 
+    if (gameBoard.currentState[1] && gameBoard.currentState[4] && gameBoard.currentState[7]) {
+      if (gameBoard.currentState[1] === gameBoard.currentState[4] && gameBoard.currentState[1] === gameBoard.currentState[7]) {
+        console.log('game over');
+        let winner = checkWinner(1);
+        announceWinner(winner);
+        gameBoard.resetBoard();
+        return;
+        return;
+      }
+    } 
 
-    // if (gameBoard.currentState[2] && gameBoard.currentState[5] && gameBoard.currentState[8]) {
-    //   if (gameBoard.currentState[2] === gameBoard.currentState[5] && gameBoard.currentState[2] === gameBoard.currentState[8]) {
-    //     console.log('game over');
-    //   }
-    // } 
+    if (gameBoard.currentState[2] && gameBoard.currentState[5] && gameBoard.currentState[8]) {
+      if (gameBoard.currentState[2] === gameBoard.currentState[5] && gameBoard.currentState[2] === gameBoard.currentState[8]) {
+        console.log('game over');
+        let winner = checkWinner(2);
+        announceWinner(winner);
+        gameBoard.resetBoard();
+        return;
+        return;
+      }
+    } 
 
-    // if (gameBoard.currentState[0] && gameBoard.currentState[4] && gameBoard.currentState[8]) {
-    //   if (gameBoard.currentState[0] === gameBoard.currentState[4] && gameBoard.currentState[0] === gameBoard.currentState[8]) {
-    //     console.log('game over');
-    //   }
-    // } 
+    if (gameBoard.currentState[0] && gameBoard.currentState[4] && gameBoard.currentState[8]) {
+      if (gameBoard.currentState[0] === gameBoard.currentState[4] && gameBoard.currentState[0] === gameBoard.currentState[8]) {
+        console.log('game over');
+        let winner = checkWinner(0);
+        announceWinner(winner);
+        gameBoard.resetBoard();
+        return;
+        return;
+      }
+    } 
 
-    // if (gameBoard.currentState[2] && gameBoard.currentState[4] && gameBoard.currentState[6]) {
-    //   if (gameBoard.currentState[2] === gameBoard.currentState[4] && gameBoard.currentState[2] === gameBoard.currentState[6]) {
-    //     console.log('game over');
-    //   }
-    // } 
+    if (gameBoard.currentState[2] && gameBoard.currentState[4] && gameBoard.currentState[6]) {
+      if (gameBoard.currentState[2] === gameBoard.currentState[4] && gameBoard.currentState[2] === gameBoard.currentState[6]) {
+        console.log('game over');
+        let winner = checkWinner(2);
+        announceWinner(winner);
+        gameBoard.resetBoard();
+        return;
+        return;
+      }
+    } 
   }
 
   return {registerMove};
